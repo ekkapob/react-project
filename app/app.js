6101b9e90183721f5ba746c2f752dcbfcb240e34
Ekkapob <ekkapob@gmail.com>
2015-12-08 17:56:24 +0700

    update

--- app/app.js
+++ app/app.js
@@ -1,44 +1,60 @@
 import React from 'react';
 import { render } from 'react-dom';
-// import { createHistory, useBasename } from 'history';
-import { Router, Route, Link } from 'react-router';
-// import Greeting from "./greeting";
-
-// React.render(<Greeting name="World"/>, document.body);
-
-// const history = useBasename(createHistory)({
-//  basename: '/'
-// });
+import { IndexRoute, Router, Route, Redirect, Link } from 'react-router';
 
 const App = React.createClass({
   render() {
     return (
       <div>
-        <p>This is home..123.</p>
-        <Link to="/about">About</Link>
+        <h1>App</h1>
+        <ul>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/inbox">Inbox</Link></li>
+        </ul>
+        {this.props.children}
       </div>
-    );
+    )
   }
-});
+})
 
 const About = React.createClass({
   render() {
+    return <h3>About</h3>
+  }
+})
+
+const Inbox = React.createClass({
+  render() {
     return (
       <div>
-        <p>This is about.</p>
-        <Link to="/">Home</Link>
+        <h2>Inbox</h2>
+        {this.props.children || "Welcome to your Inbox"}
       </div>
-    );
+    )
   }
-});
+})
 
-const routes = (
+const Message = React.createClass({
+  render() {
+    return <h3>Message {this.props.params.id}</h3>
+  }
+})
+
+const Dashboard = React.createClass({
+  render() {
+    return <div>Welcome to the app!</div>
+  }
+})
+
+render((
   <Router>
     <Route path="/" component={App}>
+      <IndexRoute component={Dashboard} />
       <Route path="about" component={About} />
+      <Route path="inbox" component={Inbox} onEnter={()=> console.log("Entering...inbox")}>
+        <Route path="/messages/:id" component={Message} onLeave={()=> console.log("leaving...messages")} />
+        <Redirect from="messages/:id" to="/messages/:id" />
+      </Route>
     </Route>
   </Router>
-);
-
-render(routes, document.getElementById('react'));
-
+), document.body);
